9a0ea8322c4c6caaf6a928913dc9fb62aecfcc99
sharadkute <96901154+sharad0671995@users.noreply.github.com>
2024-02-08 23:30:51 +0530

    New Record Project Work Started...............

---
+++ myRecord/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myRecord.Models
+{
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string City
+        {
+            get; set;
+        }
+    }
+}
